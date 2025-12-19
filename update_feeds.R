fe6421e15c311687f7ac43a100f1aa33a06e8488
rfsaldanha <rfsaldanha@gmail.com>
2025-12-19 11:10:15 -0300

    Update messages

--- update_feeds.R
+++ update_feeds.R
@@ -1,4 +1,8 @@
+cli::cli_h1("Update OCS feeds procedure")
+cli::cli_alert_info("Update job start: {lubridate::now()}")
+
 # Setup
+cli::cli_alert("Configuring setup environment...")
 # setwd("/mnt/data/onedrive/projetos/ocs_feed/")
 setwd("/dados/home/rfsaldanha/ocs_feed/")
 
@@ -11,9 +15,13 @@ mun_feeds <- readRDS("mun_feeds.rds")
 # ref_time <- readRDS(file = "last_send_time.rds")
 
 # Run collectors
+cli::cli_alert("Running collectors...")
+cli::cli_inform("INMET collector...")
 inmet_entries <- collect_inmet(last_n = 10)
+cli::cli_alert_success("INMET collector done!")
 
 # Bind all entries
+cli::cli_alert("Binding all entries...")
 entries <- tibble::tibble()
 entries <- dplyr::bind_rows(entries, inmet_entries)
 
@@ -21,26 +29,34 @@ if (nrow(entries) == 0) {
   stop("No new entries")
 }
 
-message(paste(nrow(entries), "entries."))
+cli::cli_inform("{nrow(entries)} entries")
+
 # Join mun feeds
+cli::cli_alert("Join municipality feed codes...")
 entries <- dplyr::left_join(entries, mun_feeds, by = "code_muni") |>
   dplyr::select(-code_muni)
 
 # Convert to list
+cli::cli_alert("Convert to list...")
 entries <- entries |>
   dplyr::group_by(feed) |>
   dplyr::group_split() |>
   purrr::map(.f = purrr::transpose)
 
 # Write feeds
+cli::cli_alert("Write feed files...")
 fs::dir_create(path = "/dados/home/rfsaldanha/ocs_feed/feeds")
 res <- purrr::map(.x = entries, .f = write_feed, .progress = TRUE)
 
 # Move feed files to webserver
+cli::cli_alert("Move feed files to webserver dir...")
 fs::file_move(
   path = "feeds/",
   new_path = "/dados/htdocs/shiny.icict.fiocruz.br/ocs/"
 )
 
 # Save last send time
-saveRDS(object = Sys.time(), file = "last_send_time.rds")
+# saveRDS(object = Sys.time(), file = "last_send_time.rds")
+
+cli::cli_alert_info("Update job end: {lubridate::now()}")
+cli::cli_h1("END")
