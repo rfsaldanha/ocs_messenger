937cf0e7f71954cc7561b7d1a0efd49c7eb2f92e
rfsaldanha <rfsaldanha@gmail.com>
2025-12-23 13:48:24 -0300

    Remove comma

--- collect_monitorar_saude.R
+++ collect_monitorar_saude.R
@@ -56,7 +56,7 @@ collect_monitorar_saude <- function() {
         <li>Seguir orientações médicas e procurar atendimento em caso de falta de ar, tosse persistente ou piora de sintomas respiratórios.</li>
         </ul>
         <br>
-        <i>Alerta emitido pelo Observatório de Clima e Saúde (LIS/ICICT/Fiocruz), à partir de dados do Copernicus Atmosphere Monitoring Service (CAMS).</i>        
+        <i>Alerta emitido pelo Observatório de Clima e Saúde (LIS/ICICT/Fiocruz) à partir de dados do Copernicus Atmosphere Monitoring Service (CAMS).</i>        
         "
       )
     ) |>
